ca27334d5b3979976908bf6a2b6117b69c1a4a2c
UNfassbarer <djonas2810@gmail.com>
2026-03-11 19:06:48 +0100

    new

---
+++ testmod models/ardenimium_dragon_animation.java
@@ -0,0 +1,33 @@
+// Save this class in your mod and generate all required imports
+
+/**
+ * Made with Blockbench 4.10.4
+ * Exported for Minecraft version 1.19 or later with Mojang mappings
+ * @author Author
+ */
+public class ardenimium_dragonAnimation {
+	public static final AnimationDefinition walk = AnimationDefinition.Builder.withLength(5.0F).looping()
+		.addAnimation("frontleg1", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(1.9583F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.CATMULLROM)
+		))
+		.addAnimation("frontlegtip1", new AnimationChannel(AnimationChannel.Targets.SCALE, 
+			new Keyframe(0.0F, KeyframeAnimations.scaleVec(1.0F, 1.0F, 1.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("frontleg", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
+			new Keyframe(0.2083F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(1.3333F, KeyframeAnimations.degreeVec(-3.9F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(2.5417F, KeyframeAnimations.degreeVec(4.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(4.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("frontleg", new AnimationChannel(AnimationChannel.Targets.POSITION, 
+			new Keyframe(0.5833F, KeyframeAnimations.posVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(2.25F, KeyframeAnimations.posVec(0.0F, -1.18F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(4.9583F, KeyframeAnimations.posVec(0.0F, -0.1F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.addAnimation("frontleg", new AnimationChannel(AnimationChannel.Targets.SCALE, 
+			new Keyframe(0.2917F, KeyframeAnimations.scaleVec(1.0F, 1.0F, 1.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(0.625F, KeyframeAnimations.scaleVec(1.0F, 1.0F, 1.0F), AnimationChannel.Interpolations.LINEAR),
+			new Keyframe(1.9167F, KeyframeAnimations.scaleVec(1.0F, 1.0F, 1.0F), AnimationChannel.Interpolations.LINEAR)
+		))
+		.build();
+}
